423bed47043e17f706e22ebfc2de0e6933855056
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-18 22:55:50 +0900

    update scheme definition

--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 const {ApolloServer,gql} = require('apollo-server')
 
+// HackerNewsの各投稿
+let links = [
+  {
+    id: 'link-0',
+    description: 'learn something on udemy',
+    url: 'www.something.com'
+  },
+]
+
 //GraphQLのスキーマ定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    if: ID!
+    description: String!
+    url: String!
   }
 `
 //リゾルバ関数 型に情報を入れる
 const resolvers = {
   Query: {
     info: () => 'News',
+    feed: () => links,
   }
 }
 
