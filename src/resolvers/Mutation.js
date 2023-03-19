d98722e526678432b392d30e879e5c88644b2d21
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-19 22:44:28 +0900

    create signup resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,8 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// 署名(ランダム文字列)するための秘密鍵的なもの
+APP_SECRET = 'Graphql'
 
 // ユーザの新規登録のリゾルバ
 async function signup(parent, args, context) {
@@ -12,4 +16,11 @@ async function signup(parent, args, context) {
       password,
     }
   })
+  // userのidを使ってランダム文字列(トークン)を作成
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  
+  return {
+    token,
+    user
+  }
 }
