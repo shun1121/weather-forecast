ef88b5781f830d0aca9d4f1b29c20ee14fb00bd0
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-19 17:19:52 +0900

    access database from server

--- src/server.js
+++ src/server.js
@@ -2,32 +2,28 @@ const {ApolloServer,gql} = require('apollo-server')
 const fs = require('fs')
 const path = require('path') //どこにschema.graphqlがあるか
 
-// HackerNewsの各投稿
-let links = [
-  {
-    id: 'link-0',
-    description: 'learn something on udemy',
-    url: 'www.something.com'
-  },
-]
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 //リゾルバ関数 型に情報を入れる
 const resolvers = {
   Query: {
     info: () => 'News',
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      // linkはモデル名
+      return context.prisma.link.findMany()
+    },
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      })
+      return newLink
     }
   }
 }
@@ -36,6 +32,10 @@ const server = new ApolloServer({
   //__dirnameは現在のディレクトリ、つまりsrc/, その配下のschema.graphql
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
+  // 今回はresolver内でprismaが使えるようにする。データの取得など可能になる。
+  context: {
+    prisma,
+  }
 })
 
 server.listen().then(({ url }) => console.log(`${url}でサーバ起動`))
