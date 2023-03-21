816fe36a562908567eeb01affcac14b8855b294a
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-22 01:32:19 +0900

    set up subscription resolver on server side

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,15 @@
 function newLinkSubscribe(parent, args, context) {
-  
+  // 非同期で何度も繰り返す。引数はトリガーの名前。sub側(サーバ側)の設定
+  return context.pubsub.asyncIterator('NEW_LINK')
+}
+
+const newLink = {
+  subscribe: newLinkSubscribe,
+  resolve: (payload) => {
+    return payload
+  }
+}
+
+module.exports = {
+  newLink,
 }
