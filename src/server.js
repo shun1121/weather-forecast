c4b51c8c795e628cf67bbccb51aa05378daa32f9
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-27 22:25:24 +0900

    delete unused import

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const {ApolloServer,gql} = require('apollo-server')
+const {ApolloServer} = require('apollo-server')
 const fs = require('fs')
 const path = require('path') //どこにschema.graphqlがあるか
 
